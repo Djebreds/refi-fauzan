b8d9de19a71bf4df39042f9576f442f1574e96f8
Djebreds <refiahmad.djebreds@gmail.com>
2023-05-31 05:26:40 +0700

    Setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Poppins'],
+      },
+      colors: {
+        'cv-dark': '#303642',
+        'cv-dark-gray': '#3D3D3D',
+        'cv-light-gray': '#737373',
+        'cv-blue-primary': '#38B6FF',
+        'cv-blue-primary-dark': '#1b8dcf',
+      },
+      screens: {
+        '2x1': '1320px',
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
+  content: ['./node_modules/flowbite/**/*.js'],
 };
